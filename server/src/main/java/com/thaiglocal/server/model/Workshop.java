9a11202d47e514e8d4eaaaa4fee28c3ab3e90dba
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-03-06 19:47:45 +0700

    remove unused timeTable field from Workshop model

--- server/src/main/java/com/thaiglocal/server/model/Workshop.java
+++ server/src/main/java/com/thaiglocal/server/model/Workshop.java
@@ -24,7 +24,6 @@ public class Workshop {
 
     private String workshopName;
     private String description;
-    private String timeTable;
     private Double price;
     private Integer MemberCapacity;
     private String workshopType;
