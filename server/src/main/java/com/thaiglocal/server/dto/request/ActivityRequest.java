54a245213c105bddcc410f6a77d8ff42e8b40523
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-03-08 03:31:46 +0700

    feat: update validation annotations in ActivityRequest to use @NotNull for date fields

--- server/src/main/java/com/thaiglocal/server/dto/request/ActivityRequest.java
+++ server/src/main/java/com/thaiglocal/server/dto/request/ActivityRequest.java
@@ -22,17 +22,17 @@ public class ActivityRequest {
     @NotBlank(message = "Activity name is required")
     private String activityName;
 
-    @NotBlank(message = "Start date is required")
+    @NotNull(message = "Start date is required")
     @FutureOrPresent(message = "Start date must be in the present or future")
     private LocalDateTime startDate;
 
-    @NotBlank(message = "End date is required")
+    @NotNull(message = "End date is required")
     @Future(message = "End date must be in the future")
     private LocalDateTime endDate;
 
     private String description;
 
-    @NotBlank(message = "Date can register is required")
+    @NotNull(message = "Date can register is required")
     @Future(message = "Date can register must be in the future")
     private LocalDateTime dateCanRegister;
 
