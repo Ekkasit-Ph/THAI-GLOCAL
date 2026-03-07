2e8bb03522ce4fbd454af5aa3a5f1a688f0eacd0
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-03-08 01:53:27 +0700

    feat: add validation annotations to ActivityRequest and WorkshopCreateRequest DTOs for required fields

--- server/src/main/java/com/thaiglocal/server/dto/request/ActivityRequest.java
+++ server/src/main/java/com/thaiglocal/server/dto/request/ActivityRequest.java
@@ -2,6 +2,8 @@ package com.thaiglocal.server.dto.request;
 
 import java.time.LocalDateTime;
 
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +16,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 public class ActivityRequest {
+    @NotBlank(message = "Activity name is required")
     private String activityName;
+    @NotBlank(message = "Start date is required")
     private LocalDateTime startDate;
+    @NotBlank(message = "End date is required")
     private LocalDateTime endDate;
     private String description;
+    @NotBlank(message = "Date can register is required")
     private LocalDateTime dateCanRegister;
+    @NotBlank(message = "Price is required")
     private Double price;
+    @NotBlank(message = "Register capacity is required")
     private Integer registerCapacity;
 }
